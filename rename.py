2c3c04211550b6b050b5eff7b3daf399d9132611
Evan <bakerevanj@gmail.com>
2018-02-07 15:59:05 -0500

    Modularize the renaming logic

--- rename.py
+++ rename.py
@@ -3,8 +3,11 @@ import os, random
 nums = list(range(len(os.listdir('test'))))
 random.shuffle(nums)
 
-for i, f in enumerate(os.listdir('test')):
-	extention = os.path.splitext(f)[1]
-	oldFile = f
-	newFile = "%d%s"%(nums[i], extention)
-	os.rename('test/' + oldFile, 'test/' + newFile)
+def rename(newNames):
+	for i, f in enumerate(os.listdir('test')):
+		extention = os.path.splitext(f)[1]
+		oldFile = f
+		newFile = "%d%s"%(newNames[i], extention)
+		os.rename('test/' + oldFile, 'test/' + newFile)
+
+rename(nums)
