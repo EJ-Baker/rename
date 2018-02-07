fed5901623c74500ff996a970fb1b016e79b682e
Evan <bakerevanj@gmail.com>
2018-02-07 15:47:04 -0500

    Made loop more pythonic

--- rename.py
+++ rename.py
@@ -3,8 +3,8 @@ import os, random
 nums = list(range(len(os.listdir('test'))))
 random.shuffle(nums)
 
-for f in range(len(os.listdir('test'))):
-	extention = os.path.splitext(os.listdir('test')[f])[1]
-	oldFile = os.listdir('test')[f]
-	newFile = "%d%s"%(nums[f], extention)
+for i, f in enumerate(os.listdir('test')):
+	extention = os.path.splitext(f)[1]
+	oldFile = f
+	newFile = "%d%s"%(nums[i], extention)
 	os.rename('test/' + oldFile, 'test/' + newFile)
