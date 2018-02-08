7280769b5a07b4c897519ef89b1c28ff997a238a
Evan <bakerevanj@gmail.com>
2018-02-08 16:11:48 -0500

    Add padding functions for int and hex, eg (9, 10) is now (09, 10)

--- nameArray.py
+++ nameArray.py
@@ -8,6 +8,16 @@ def int_array_random(size, offset):
     random.shuffle(array)
     return array
 
+def int_array_padded(size, offset):
+    array = int_array(size, offset)
+    length = len(str(size + offset))
+    return (['{num:0{width}}'.format(num = i , width = length) for i in array])
+
+def int_array_random_padded(size, offset):
+    array = int_array_random(size, offset)
+    length = len(str(size + offset))
+    return (['{num:0{width}}'.format(num = i , width = length) for i in array])
+
 def hex_array(size, offset):
     array = int_array(size, offset)
     return ([hex(i)[2:] for i in array])
@@ -15,3 +25,13 @@ def hex_array(size, offset):
 def hex_array_random(size, offset):
     array = int_array_random(size, offset)
     return ([hex(i)[2:] for i in array])
+
+def hex_array_padded(size, offset):
+    array = hex_array(size, offset)
+    length = len(hex(size + offset)[2:])
+    return (['{:0>{width}}'.format(i , width = length) for i in array])
+
+def hex_array_random_padded(size, offset):
+    array = hex_array_random(size, offset)
+    length = len(hex(size + offset)[2:])
+    return (['{:0>{width}}'.format(i , width = length) for i in array])
