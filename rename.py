88adfd9377f9c17f851749a8214f48bbeb2d533d
Evan <bakerevanj@gmail.com>
2018-02-08 16:42:50 -0500

    Add prefix/suffix support and fix bug in renaming not working with strings

--- rename.py
+++ rename.py
@@ -1,16 +1,18 @@
 import os, sys
 import nameArray
 
-def rename(newNames):
+def rename(newNames, prefix, suffix):
 	for i, f in enumerate(os.listdir()):
 		extention = os.path.splitext(f)[1]
 		oldFile = f
-		newFile = "%d%s"%(newNames[i], extention)
+		newFile = "{}{}{}".format(prefix, newNames[i], suffix)
 		os.rename(oldFile, newFile)
 
 if __name__ == '__main__':
 	os.chdir(sys.argv[1])
+	prefix = sys.argv[2]
+	suffix = sys.argv[3]
 
-	nums = nameArray.int_array_random(len(os.listdir()), 10)
+	nums = nameArray.hex_array_random_padded(len(os.listdir()), 7)
 
-	rename(nums)
+	rename(nums, prefix, suffix)
