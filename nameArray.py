7a82b9f8a341952caf7954ce27c5617dc3466a06
Evan <bakerevanj@gmail.com>
2018-02-08 12:34:13 -0500

    Add functions for hex arrays without the 0x prefix

--- nameArray.py
+++ nameArray.py
@@ -2,7 +2,16 @@ import random
 
 def int_array(size):
     return list(range(0, size))
+
 def int_array_random(size):
     array = int_array(size)
     random.shuffle(array)
     return array
+
+def hex_array(size):
+    array = int_array(size)
+    return ([hex(i)[2:] for i in array])
+
+def hex_array_random(size):
+    array = int_array_random(size)
+    return ([hex(i)[2:] for i in array])
