2be9d390b6e5b3fa73530ff1fb60a0132b2e097e
Evan <bakerevanj@gmail.com>
2018-02-10 14:00:33 -0500

    Implement logic to handle all commands

--- rename.py
+++ rename.py
@@ -11,14 +11,47 @@ def rename(newNames, prefix, suffix):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description = "Renames all files in a directory")
 	parser.add_argument('dir', nargs = '?', default = os.getcwd(), help = 'the directory to rename, default cwd')
-	parser.add_argument('-pre', '-prefix', nargs = 1, default = '', help = 'a string to prepend the files', metavar = 'PREFIX')
-	parser.add_argument('-s', '-suffix', nargs = 1, default = '', help = 'a string to append the files', metavar = 'SUFFIX')
+	parser.add_argument('-pre', '-prefix', default = '', help = 'a string to prepend the files', metavar = 'PREFIX')
+	parser.add_argument('-suf', '-suffix', default = '', help = 'a string to append the files', metavar = 'SUFFIX')
+
 	group = parser.add_mutually_exclusive_group()
 	group.add_argument('-i', '-int', action = 'store_true', help = 'renames the files as a series of ints')
 	group.add_argument('-x', '-hex', action = 'store_true', help = 'renames the files as a series of hexes')
+
 	parser.add_argument('-r', '-random', action = 'store_true', help = 'randomizes the order of the files, dependent on -i or -x')
 	parser.add_argument('-p', '-pad', action = 'store_true',
 		help = 'pads -i or -x with zeros to keep names the same length, dependent on -i or -x')
-	parser.add_argument('-o', '-offset', nargs = 1, default = 0, type = int,
+	parser.add_argument('-o', '-offset', default = 0, type = int,
 		help = 'adds an offset for -i or -x to start from, dependent on -i or -x', metavar = 'OFFSET')
+
 	args = parser.parse_args()
+
+	if not (args.i or args.x) and (args.r or args.p or args.o):
+		parser.error('-r -p -o are dependent on -i or -x')
+	if not (args.o >= 0):
+		parser.error('-o must be a positive int')
+
+	names = []
+	size = len(os.listdir(args.dir))
+	offset = args.o
+	if args.i:
+		if args.r and args.p:
+			names = nameArray.int_array_random_padded(size, offset)
+		elif args.r:
+			names = nameArray.int_array_random(size, offset)
+		elif args.p:
+			names = nameArray.int_array_padded(size, offset)
+		else:
+			names = nameArray.int_array(size, offset)
+	elif args.x:
+		if args.r and args.p:
+			names = nameArray.hex_array_random_padded(size, offset)
+		elif args.r:
+			names = nameArray.hex_array_random(size, offset)
+		elif args.p:
+			names = nameArray.hex_array_padded(size, offset)
+		else:
+			names = nameArray.hex_array(size, offset)
+
+	os.chdir(args.dir)
+	rename(names, args.pre, args.suf)
