ddd55817eecc7efe36ccbbe3b9b01a0b264ca31f
Evan <bakerevanj@gmail.com>
2018-02-09 22:22:22 -0500

    Add the definitions for some of the commands

--- rename.py
+++ rename.py
@@ -10,4 +10,10 @@ def rename(newNames, prefix, suffix):
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description = "Renames all files in a directory")
-	parser.print_help()
+	parser.add_argument('dir', default = '', help = 'the directory to rename, default cwd')
+	parser.add_argument('-p', '-prefix', nargs = 1, default = '', help = 'a string to prepend the files', metavar = 'PREFIX')
+	parser.add_argument('-s', '-suffix', nargs = 1, default = '', help = 'a string to append the files', metavar = 'SUFFIX')
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('-i', '-int', action = 'store_true', help = 'renames the files as a series of ints')
+	group.add_argument('-x', '-hex', action = 'store_true', help = 'renames the files as a series of hexes')
+	args = parser.parse_args()
